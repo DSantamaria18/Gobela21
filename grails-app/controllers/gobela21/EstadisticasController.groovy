09d9ed1ff1461fe55e83e8a26f4d902570b329ea
David Santamaría <dsantamarialago@ticketbis.com>
2017-04-02 08:31:01 +0200

    mejoras en el aspecto del excel

--- grails-app/controllers/gobela21/EstadisticasController.groovy
+++ grails-app/controllers/gobela21/EstadisticasController.groovy
@@ -1,9 +1,8 @@
 package gobela21
 
+import jxl.*
 import jxl.Workbook
-import jxl.write.Label
-import jxl.write.WritableSheet
-import jxl.write.WritableWorkbook
+import jxl.write.*
 
 class EstadisticasController {
     def InformesService
@@ -13,48 +12,84 @@ class EstadisticasController {
         [resultList: resultList]
     }
 
-    def exportar() {
+    def exportarInformeSub() {
         def resultList = InformesService.subvencionesPorLinea()
         response.setContentType('application/vnd.ms-excel')
         response.setHeader('Content-Disposition', 'Attachment;Filename="subvenciones.xls"')
         WritableWorkbook workbook = Workbook.createWorkbook(response.outputStream)
-        for (int i = 0; i < resultList.size(); i++) {
-            String nombreHoja = "Linea ${i + 1}"
-            def datosLinea = resultList[i]
-            if (datosLinea.size() > 0) {
-                WritableSheet sheet = workbook.createSheet(nombreHoja, i)
-                sheet.addCell(new Label(0, 0, "SUBVENCIONES LINEA ${i + 1}"))
-                def cabeceras = datosLinea[0].keySet()
-                for (int j = 0; j < cabeceras.size(); j++) {
-                    sheet.addCell(new Label(j, 3, "${cabeceras[j]}".toUpperCase()))
-                }
-                for (int z = 0; z < datosLinea.size(); z++) {
-                    def subvencion = datosLinea[z]
-                    for (int x = 0; x < subvencion.size(); x++) {
-                        sheet.addCell(new Label(x, z + 4, "${subvencion[x]}"))
+        try {
+            for (int i = 0; i < resultList.size(); i++) {
+                String nombreHoja = "Linea ${i + 1}"
+                def datosLinea = resultList[i]
+                if (datosLinea.size() > 0) {
+                    WritableSheet sheet = workbook.createSheet(nombreHoja, i)
+                    WritableFont titleFont = new WritableFont(WritableFont.ARIAL, 16, WritableFont.BOLD)
+                    WritableCellFormat titleFormat = new WritableCellFormat()
+                    titleFormat.setFont(titleFont)
+
+                    WritableFont headerFont = new WritableFont(WritableFont.ARIAL, 11, WritableFont.BOLD)
+                    WritableCellFormat headerFormat = new WritableCellFormat()
+                    headerFormat.with {
+                        setBackground(Colour.GREY_25_PERCENT)
+                        setBorder(Border.ALL, BorderLineStyle.THIN)
+                        setFont(headerFont)
+                        setWrap(true)
+                    }
+
+                    WritableFont cellFont = new WritableFont(WritableFont.ARIAL, 10)
+                    WritableCellFormat cellFormat = new WritableCellFormat()
+                    cellFormat.with {
+                        setFont(cellFont)
+                        setBorder(Border.ALL, BorderLineStyle.THIN)
+                        setWrap(true)
+                    }
+
+                    /*WritableCellFormat currencyFormat = new WritableCellFormat(new NumberFormat(NumberFormat.))
+                    currencyFormat.with{
+                        setFont(cellFont)
+                        setBorder(Border.ALL, BorderLineStyle.THIN)
+                    }*/
+
+                    sheet.addCell(new Label(0, 0, "SUBVENCIONES LINEA ${i + 1}", titleFormat))
+                    def cabeceras = datosLinea[0].keySet()
+
+                    for (int j = 0; j < cabeceras.size(); j++) {
+                        sheet.addCell(new Label(j, 3, cabeceras[j].toUpperCase(), headerFormat))
+                    }
+                    for (int z = 0; z < datosLinea.size(); z++) {
+                        def subvencion = datosLinea[z]
+                        for (int x = 0; x < subvencion.size(); x++) {
+                            if (subvencion[x] && subvencion[x].getClass() == String) {
+                                sheet.addCell(new Label(x, z + 4, subvencion[x], cellFormat))
+                            } else {
+                                if (subvencion[x].getClass() == BigDecimal) {
+                                    sheet.addCell(new Number(x, z + 4, subvencion[x], cellFormat))
+                                    //sheet.addCell(new Number(x, z + 4, subvencion[x], currencyFormat))
+                                } else {
+                                    if (subvencion[x]) sheet.addCell(new Number(x, z + 4, subvencion[x], cellFormat))
+                                }
+                            }
+                        }
+                    }
+                    for (int n = 0; n < datosLinea[0].size() - 2; n++) {
+                        sheet.addCell(new Label(n, datosLinea.size() + 4, "", headerFormat))
+                    }
+                    int colum = datosLinea[0].size() - 1
+                    int rowIni = 5
+                    int rowFin = rowIni + datosLinea.size() - 1
+                    sheet.addCell(new Label(datosLinea[0].size() - 2, datosLinea.size() + 4, "TOTAL:", headerFormat))
+                    Formula total = new Formula(colum, rowFin, "SUMA(C${rowIni}:C${rowFin})", headerFormat)
+                    sheet.addCell(total)
+
+                    for (int c = 0; c < 10; c++) {
+                        sheet.setColumnView(c, 20)
                     }
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace()
         }
-        /* WritableSheet sheet1 = workbook.createSheet("Students", 0)
-         sheet1.addCell(new Label(0,0, "First Name"))
-         sheet1.addCell(new Label(1,0, "Last Name"))
-         sheet1.addCell(new Label(2,0, "Age"))
-         sheet1.addCell(new Label(0,1, "John"))
-         sheet1.addCell(new Label(1,1, "Doe"))
-         sheet1.addCell(new Label(2,1, "20"))
-         sheet1.addCell(new Label(0,2, "Jane"))
-         sheet1.addCell(new Label(1,2, "Smith"))
-         sheet1.addCell(new Label(2,2, "18"))
-         WritableSheet sheet2 = workbook.createSheet("Courses", 1)
-         sheet2.addCell(new Label(0,0, "Course Name"))
-         sheet2.addCell(new Label(1,0, "Number of units"))
-         sheet2.addCell(new Label(0,1, "Algebra"))
-         sheet2.addCell(new Label(1,1, "3"))
-         sheet2.addCell(new Label(0,2, "English Grammar"))
-         sheet2.addCell(new Label(1,2, "5"))*/
 
-        jxlUtils.getWCell()
         workbook.write()
         workbook.close()
     }
